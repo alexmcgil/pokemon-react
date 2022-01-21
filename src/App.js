aeed414b1a689882bba26899e8afa23a3ab1d34b
Alex <alexmcgil@vivaldi.net>
2022-01-21 18:25:34 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ContainerReact from "./Components/ContainerReact";
+import {Container} from "@mui/material";
+
+
+function App(){
+
+	return <Container fixed>
+		<ContainerReact />
+	</Container>
+}
+
+export default App;
---
+++ src/Components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Box} from "@mui/material";
+
+
+function Title(){
+
+    return <>
+        <Box ></Box>
+    </>
+}
+
+export default Title;
